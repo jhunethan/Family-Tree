6ccd18bf7e35cf9ebf3238e00f90d40855ba274e
jhunethan <jhunethan2812@gmail.com>
2021-02-03 22:11:06 +0000

    header layout

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "../css/LandingPage.css";
 import { Link } from "react-router-dom";
-import image from "../css/linh-tran-VIvGPBm6uxg-unsplash.jpg";
 
 export default function LandingPage() {
   return (
     <div className="wrapper">
-      <div className="image-container">
-        <img src={image} alt="landing-background" />
-      </div>
       <section className="content-container">
         <div className="header">
           <h1>Lay Family Database</h1>
@@ -28,14 +24,7 @@ export default function LandingPage() {
         <section className="about-section">
           <div className="stat-card users"></div>
           <div className="stat-card members"></div>
-          <div className="edit-history">
-            Reprehenderit nisi Lorem sint pariatur consequat enim ut enim culpa.
-            Laboris exercitation proident nulla duis eiusmod do dolore est
-            consequat non laborum. Minim consequat fugiat laboris id mollit qui
-            mollit fugiat pariatur sint aliquip sint do qui. Mollit enim nostrud
-            id nulla est. Elit fugiat ex Lorem aute occaecat dolore sint.
-            Proident sit do proident duis nostrud. Mollit et veniam sint in.
-          </div>
+          <div className="edit-history"> </div>
         </section>
       </section>
     </div>
