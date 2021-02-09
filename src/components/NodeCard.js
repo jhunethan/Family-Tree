fb711a800900aa9d68f8342b064152805035c513
jhunethan <jhunethan2812@gmail.com>
2021-02-09 22:29:37 +0000

    upload and view user photo single

--- src/components/NodeCard.js
+++ src/components/NodeCard.js
@@ -5,244 +5,27 @@ import { Image } from "cloudinary-react";
 
 import "../css/NodeCard.css";
 import placeholder from "../css/person-placeholder.jpg";
+import { NodeCardDetails, ImmediateFamily } from "./NodeCardContent";
+import { useCookies } from "react-cookie";
 
-const getNode = (name, data) => {
-  let tempname;
-  for (let i = 0; i < data.length; i++) {
-    tempname = data[i].generation + " " + data[i].name;
-    if (name === tempname || name === data[i].name) {
-      return data[i];
-    }
-  }
-};
-
-const getChildren = (id, data) => {
-  let arr = [];
-  if (Number(id) === 0) return arr;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].pid === Number(id) && data[i].isPartner !== 1) {
-      arr.push(data[i]);
-    }
-  }
-  if (arr.length > 0) return arr;
-};
-
-function NodeCardDetails(props) {
-  switch (props.method) {
-    case "birthdate":
-      try {
-        if (
-          props.node.extradetails.birthplace !== "" &&
-          props.node.birthdate !== ""
-        ) {
-          return (
-            <section>
-              <h2>Born</h2>
-              <p>
-                {props.node.birthdate} in {props.node.extradetails.birthplace}
-              </p>
-            </section>
-          );
-        } else if (props.node.extradetails.birthplace !== "") {
-          return (
-            <section>
-              <h2>Born</h2>
-              <p>in {props.node.extradetails.birthplace}</p>
-            </section>
-          );
-        }
-      } catch {}
+function MemberPhotos(props) {
+  try {
+    if (props.node.extradetails.photo_id) {
       return (
-        <section>
-          <h2>Born</h2>
-          <p>{props.node.birthdate}</p>
-        </section>
+        <Image
+          cloudName="dqwu1p8fp"
+          public_id={props.node.extradetails.photo_id}
+        />
       );
-    case "generation":
-      if (props.node.generation !== "") {
-        return (
-          <section>
-            <h2>Generation</h2>
-            <p>{props.node.generation}</p>
-          </section>
-        );
-      } else {
-        return null;
-      }
-    case "location":
-      try {
-        if (props.node.extradetails.location !== "") {
-          return (
-            <section>
-              <h2>Current location</h2>
-              <p>{props.node.extradetails.location}</p>
-            </section>
-          );
-        } else return null;
-      } catch {
-        return null;
-      }
-    case "extranames":
-      try {
-        let extranames = props.node.extradetails.extranames;
-
-        if (extranames !== "") {
-          return (
-            <section>
-              <h2>Additional Names</h2>
-              <p>{extranames}</p>
-            </section>
-          );
-        } else return null;
-      } catch {
-        return null;
-      }
-    case "fblink":
-      try {
-        if (
-          props.node.extradetails.fblink.includes("https://www.facebook.com")
-        ) {
-          return (
-            <section>
-              <a className="fblink" href={props.node.extradetails.fblink}>
-                Facebook Link
-              </a>
-            </section>
-          );
-        } else {
-          return null;
-        }
-      } catch {
-        return null;
-      }
-    case "description":
-      try {
-        if (props.node.extradetails.description !== "") {
-          let output = props.node.extradetails.description.split("\n\n");
-          let count = 0;
-          return (
-            <section>
-              <h2>Description</h2>
-              <div>
-                {output.map((x) => {
-                  count += 1;
-                  return <p key={count}>{x}</p>;
-                })}
-              </div>
-            </section>
-          );
-        } else return null;
-      } catch {
-        return null;
-      }
-    default:
-      break;
-  }
-}
-
-function ImmediateFamily(props) {
-  switch (props.method) {
-    case "parents":
-      try {
-        let parent = getNode(props.node.parent, props.treeData);
-        let partner;
-        try {
-          partner = `${parent.partnerinfo.name} (${parent.partnerinfo.extradetails.extranames})`;
-        } catch {
-          partner = parent.partnerinfo.name;
-        }
-        if (parent.partnerinfo.name !== "") {
-          return (
-            <div className="card-parents card-related">
-              <h2>Known Parents</h2>
-              <p>{props.node.parent}</p>
-              <p>{partner}</p>
-            </div>
-          );
-        }
-      } catch {
-        if (props.node.parent !== undefined && props.node.parent !== "") {
-          return (
-            <div className="card-parents card-related">
-              <h2>Known Parents</h2>
-              <p>{props.node.parent}</p>
-            </div>
-          );
-        } else {
-          return null;
-        }
-      }
-      return null;
-    case "siblings":
-      try {
-        let parent = getNode(props.node.parent, props.treeData);
-        let siblings = getChildren(parent.id, props.treeData);
-        if (siblings.length > 1) {
-          return (
-            <div className="card-siblings card-related">
-              <h2>Known Siblings</h2>
-              {siblings.map((x) => {
-                if (x.name !== props.node.name) {
-                  return (
-                    <p key={x.id}>
-                      {x.generation} {x.name}
-                    </p>
-                  );
-                } else return <p key={x.id}></p>;
-              })}
-            </div>
-          );
-        } else return null;
-      } catch {
-        return null;
-      }
-    case "children":
-      try {
-        let node, id;
-        node = getNode(props.node.name, props.treeData);
-        node.isPartner ? (id = node.pid) : (id = node.id);
-        let children = getChildren(id, props.treeData);
-        if (children.length > 0) {
-          return (
-            <div className="card-children card-related">
-              <h2>Known Children</h2>
-              {children.map((x) => {
-                return (
-                  <p key={x.id}>
-                    {x.generation} {x.name}
-                  </p>
-                );
-              })}
-            </div>
-          );
-        }
-      } catch {
-        return null;
-      }
-      return null;
-    default:
-      return null;
-  }
+    }
+  } catch {}
+  return <img src={placeholder} alt="placeholder" />;
 }
 
 export default function NodeCard(props) {
   const [cardexpanded, setcardexpanded] = useState(false);
-  const [image, setImage] = useState(undefined);
-  const [imageServed] = useState(placeholder);
-  const [progress, setProgress] = useState("");
-
-  // useEffect(() => {
-  //   try {
-  //     Axios.get("http://localhost:5000/api/get/photos/user", {
-  //       params: { filename: `ID${props.node.id}` },
-  //       responseType: "blob",
-  //     }).then((response) => {
-  //       let imgUrl = URL.createObjectURL(response.data);
-  //       setImageServed(imgUrl);
-  //       if (response.data.type === "text/html") setImageServed(placeholder);
-  //     });
-  //   } catch (error) {}
-  // }, [props.node]);
+  const [imageToBeSent, setImageToBeSent] = useState(undefined);
+  const [cookies] = useCookies(["author"]);
 
   const transform = () => {
     if (!cardexpanded) {
@@ -260,7 +43,7 @@ export default function NodeCard(props) {
 
   const uploadImage = () => {
     const formData = new FormData();
-    formData.append("file", image);
+    formData.append("file", imageToBeSent);
     formData.append("upload_preset", "oms6f6zi");
 
     Axios.post(
@@ -269,6 +52,11 @@ export default function NodeCard(props) {
     ).then((Response) => {
       console.log(Response);
       //save it to extradetails db as filename: `${Response.data.public_id}.${Response.data.format}`
+      Axios.put("http://localhost:5000/api/updateextra", {
+        id: Number(props.node.id),
+        photo_id: Response.data.public_id,
+        author: cookies.author,
+      });
     });
   };
 
@@ -292,17 +80,16 @@ export default function NodeCard(props) {
       </div>
       <div className="card-main">
         <section className="top-card">
-          <Image cloudName="dqwu1p8fp" public_id="trxzimtewj41fn3aghia"/>
+          <MemberPhotos node={props.node} />
         </section>
         <input
           type="file"
           id="uploadfile"
           onChange={(event) => {
-            setImage(event.target.files[0]);
+            setImageToBeSent(event.target.files[0]);
           }}
         />
         <input type="submit" onClick={() => uploadImage()} />
-        {progress}
         <section className="middle-card">
           <h1 className="card-subtitle">
             {props.node.generation} {props.node.name}
---
+++ src/components/NodeCardContent.js
@@ -0,0 +1,220 @@
+import React from "react";
+
+const getNode = (name, data) => {
+  let tempname;
+  for (let i = 0; i < data.length; i++) {
+    tempname = data[i].generation + " " + data[i].name;
+    if (name === tempname || name === data[i].name) {
+      return data[i];
+    }
+  }
+};
+
+const getChildren = (id, data) => {
+  let arr = [];
+  if (Number(id) === 0) return arr;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].pid === Number(id) && data[i].isPartner !== 1) {
+      arr.push(data[i]);
+    }
+  }
+  if (arr.length > 0) return arr;
+};
+
+export function NodeCardDetails(props) {
+  switch (props.method) {
+    case "birthdate":
+      try {
+        if (
+          props.node.extradetails.birthplace !== "" &&
+          props.node.birthdate !== ""
+        ) {
+          return (
+            <section>
+              <h2>Born</h2>
+              <p>
+                {props.node.birthdate} in {props.node.extradetails.birthplace}
+              </p>
+            </section>
+          );
+        } else if (props.node.extradetails.birthplace !== "") {
+          return (
+            <section>
+              <h2>Born</h2>
+              <p>in {props.node.extradetails.birthplace}</p>
+            </section>
+          );
+        }
+      } catch {}
+      return (
+        <section>
+          <h2>Born</h2>
+          <p>{props.node.birthdate}</p>
+        </section>
+      );
+    case "generation":
+      if (props.node.generation !== "") {
+        return (
+          <section>
+            <h2>Generation</h2>
+            <p>{props.node.generation}</p>
+          </section>
+        );
+      } else {
+        return null;
+      }
+    case "location":
+      try {
+        if (props.node.extradetails.location !== "") {
+          return (
+            <section>
+              <h2>Current location</h2>
+              <p>{props.node.extradetails.location}</p>
+            </section>
+          );
+        } else return null;
+      } catch {
+        return null;
+      }
+    case "extranames":
+      try {
+        let extranames = props.node.extradetails.extranames;
+
+        if (extranames !== "") {
+          return (
+            <section>
+              <h2>Additional Names</h2>
+              <p>{extranames}</p>
+            </section>
+          );
+        } else return null;
+      } catch {
+        return null;
+      }
+    case "fblink":
+      try {
+        if (
+          props.node.extradetails.fblink.includes("https://www.facebook.com")
+        ) {
+          return (
+            <section>
+              <a className="fblink" href={props.node.extradetails.fblink}>
+                Facebook Link
+              </a>
+            </section>
+          );
+        } else {
+          return null;
+        }
+      } catch {
+        return null;
+      }
+    case "description":
+      try {
+        if (props.node.extradetails.description !== "") {
+          let output = props.node.extradetails.description.split("\n\n");
+          let count = 0;
+          return (
+            <section>
+              <h2>Description</h2>
+              <div>
+                {output.map((x) => {
+                  count += 1;
+                  return <p key={count}>{x}</p>;
+                })}
+              </div>
+            </section>
+          );
+        } else return null;
+      } catch {
+        return null;
+      }
+    default:
+      break;
+  }
+}
+
+export function ImmediateFamily(props) {
+  switch (props.method) {
+    case "parents":
+      try {
+        let parent = getNode(props.node.parent, props.treeData);
+        let partner;
+        try {
+          partner = `${parent.partnerinfo.name} (${parent.partnerinfo.extradetails.extranames})`;
+        } catch {
+          partner = parent.partnerinfo.name;
+        }
+        if (parent.partnerinfo.name !== "") {
+          return (
+            <div className="card-parents card-related">
+              <h2>Known Parents</h2>
+              <p>{props.node.parent}</p>
+              <p>{partner}</p>
+            </div>
+          );
+        }
+      } catch {
+        if (props.node.parent !== undefined && props.node.parent !== "") {
+          return (
+            <div className="card-parents card-related">
+              <h2>Known Parents</h2>
+              <p>{props.node.parent}</p>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      }
+      return null;
+    case "siblings":
+      try {
+        let parent = getNode(props.node.parent, props.treeData);
+        let siblings = getChildren(parent.id, props.treeData);
+        if (siblings.length > 1) {
+          return (
+            <div className="card-siblings card-related">
+              <h2>Known Siblings</h2>
+              {siblings.map((x) => {
+                if (x.name !== props.node.name) {
+                  return (
+                    <p key={x.id}>
+                      {x.generation} {x.name}
+                    </p>
+                  );
+                } else return <p key={x.id}></p>;
+              })}
+            </div>
+          );
+        } else return null;
+      } catch {
+        return null;
+      }
+    case "children":
+      try {
+        let node, id;
+        node = getNode(props.node.name, props.treeData);
+        node.isPartner ? (id = node.pid) : (id = node.id);
+        let children = getChildren(id, props.treeData);
+        if (children.length > 0) {
+          return (
+            <div className="card-children card-related">
+              <h2>Known Children</h2>
+              {children.map((x) => {
+                return (
+                  <p key={x.id}>
+                    {x.generation} {x.name}
+                  </p>
+                );
+              })}
+            </div>
+          );
+        }
+      } catch {
+        return null;
+      }
+      return null;
+    default:
+      return null;
+  }
+}
